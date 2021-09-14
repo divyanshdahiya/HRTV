01b94f22c7911e88fc896005b0abdeb14c4acec5
divyanshdahiya <dahiya.divyansh1@gmail.com>
2021-09-14 22:10:45 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "../styles/navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IconContext } from "react-icons";
+
+const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+      <nav className="main-nav">
+        <div className="logo">
+          <h2>HRTV</h2>
+        </div>
+
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }
+        >
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+          </ul>
+        </div>
+        <IconContext.Provider value={{ color: "white" }}>
+          <div className="hamburger">
+            <div className="hamburger-menu">
+              <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                <GiHamburgerMenu />
+              </a>
+            </div>
+          </div>
+        </IconContext.Provider>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
